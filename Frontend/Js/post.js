f58a63c79e15fd677c72c29f176da6a6c9e5f97a
Valeria Rubio Herrera <valeriarubioh@gmail.com>
2023-10-08 21:44:48 -0500

    Agrega localstorage a comunidad y post pregunta

--- Frontend/Js/post.js
+++ Frontend/Js/post.js
@@ -1,25 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    const postForm = document.querySelector('.principal__formulario')
+  const postForm = document.querySelector(".principal__formulario");
 
-    postForm.addEventListener('submit',(e)=> {
-        e.preventDefault()
-        
-        const titulo = document.getElementById('input__titulo').value;
-        const contenido = document.getElementById('input__texto').value;
-        const tags = document.getElementById('input__tags').value;
+  postForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        const nuevaPublicacion = document.createElement('div');
-        nuevaPublicacion.innerHTML = `<h3>${titulo}</h3><p>${contenido}</p><p>${tags}</p>`;
-
-        const comunidadContenedor = document.querySelector(".foro__pregunta");
-        const comunidadForo = document.querySelector(".foro__publicadas");
-        comunidadForo.insertBefore(nuevaPublicacion,comunidadContenedor);
-        //Limpiar formulario
-        document.getElementById('input__titulo').value = "";
-        document.getElementById('input__texto').value = "";
-        document.getElementById('input__tags').value = "";
-
-        window.location.href = "comunidad.html";
-    });
+    const titulo = document.getElementById("input__titulo").value;
+    const contenido = document.getElementById("input__texto").value;
+    const tags = document.getElementById("input__tags").value;
+    const Preguntas = JSON.parse(localStorage.getItem("preguntas")) || [];
+    Preguntas.push({ pregunta: titulo, descripcion: contenido, tags: tags });
+    localStorage.setItem("preguntas", JSON.stringify(Preguntas));
+    window.location.href = "comunidad.html";
+  });
 });
