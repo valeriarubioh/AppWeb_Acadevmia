94e1e1ba26a4f64be49713f8220309b296f7d475
Valeria Rubio <valeriarubioh@gmail.com>
2023-10-12 22:27:13 -0500

    login con correo, integracion inicial formulario en comunidad

--- Frontend/Js/comunidad.js
+++ Frontend/Js/comunidad.js
@@ -41,10 +41,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Verificar si el usuario ha iniciado sesión (aquí debes implementar tu lógica de autenticación)
     const user = JSON.parse(localStorage.getItem("login_success")) || false;
 
-    if (user) {
-      window.location.href = "postPregunta.html"; // Redirigir a post.html si el usuario ha iniciado sesión
-    } else {
+    // if (user) {
+    //   window.location.href = "postPregunta.html"; // Redirigir a post.html si el usuario ha iniciado sesión
+    // } else {
+    //   window.location.href = "login.html"; // Redirigir a login.html si el usuario no ha iniciado sesión
+    // }
+    if (!user) {
       window.location.href = "login.html"; // Redirigir a login.html si el usuario no ha iniciado sesión
-    }
+    }      
+    
   });
 });
