413e999c5c34a4693ceb994e2accfcfe8265aa0b
Valeria Rubio <valeriarubioh@gmail.com>
2023-10-29 18:17:09 -0500

    Creacion API spring

---
+++ Api/src/main/java/com/generation/acadevmia/AcadevmiaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.acadevmia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadevmiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadevmiaApplication.class, args);
+	}
+
+}
