d5a087021409da76e2f98b74d21631e30aa280eb
Valeria Rubio <valeriarubioh@gmail.com>
2023-11-06 16:46:53 -0500

    se optimiza imports

--- Api/src/main/java/com/generation/acadevmia/repository/ReaccionRepository.java
+++ Api/src/main/java/com/generation/acadevmia/repository/ReaccionRepository.java
@@ -1,13 +1,7 @@
 package com.generation.acadevmia.repository;
 
 import com.generation.acadevmia.entity.ReaccionEntity;
-import com.generation.acadevmia.entity.UserEntity;
-import com.generation.acadevmia.payload.request.EReaccionType;
-import com.generation.acadevmia.payload.request.ReaccionRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface ReaccionRepository extends MongoRepository<ReaccionEntity, String> {
 }
