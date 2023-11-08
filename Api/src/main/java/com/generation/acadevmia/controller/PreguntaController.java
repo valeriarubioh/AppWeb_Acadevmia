d878b172fba1400dcd66ca2ac08a8a475de6e5ce
Valeria Rubio <valeriarubioh@gmail.com>
2023-11-08 15:06:25 -0500

    Se habilita CORS para todos los origenes

--- Api/src/main/java/com/generation/acadevmia/controller/PreguntaController.java
+++ Api/src/main/java/com/generation/acadevmia/controller/PreguntaController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/preguntas")
-@CrossOrigin(origins = "http://127.0.0.1:5501")
+@CrossOrigin(origins = "*")
 public class PreguntaController {
 
     @Autowired
