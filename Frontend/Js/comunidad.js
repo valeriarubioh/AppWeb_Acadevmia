09f91abb51812502e2b15e453a88f54133f003a6
emmspy <emilysky48@gmail.com>
2023-11-08 12:09:32 -0500

    fetch cominidad

--- Frontend/Js/comunidad.js
+++ Frontend/Js/comunidad.js
@@ -1,42 +1,80 @@
-const preguntas = JSON.parse(localStorage.getItem("preguntas")) || [];
 const user = JSON.parse(localStorage.getItem("login_success")) || false;
+
+//obtener la el listado de preguntas 
+function obtenerPreguntasDesdeBackend() {
+  fetch("http://127.0.0.1:8080/api/v1/preguntas", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      preguntas = data; 
+      // Actualiza las preguntas con los datos del backend
+      renderPreguntas(); 
+      // Vuelve a renderizar las preguntas en la interfaz
+    })
+    .catch((error) => console.error("Error al obtener preguntas:", error));
+}
 
 function agregarReaccion(index, accion) {
   if (!user) {
     window.location.href = "login.html";
+  } else {
+    window.location.href = "../../index.html"
   }
+
   const pregunta = preguntas[index];
   if (pregunta.username === user.username) {
     alert("El mismo usuario no puede reaccionar a su propia publicación");
     return;
   }
-  const reaccion = pregunta.reaccion;
-  let isLike = accion === "like" ? 1 : 0;
 
-  const foundIndex = reaccion.findIndex(
-    (reaccion) => reaccion.username === user.username
-  );
-  if (foundIndex === -1) {
-    reaccion.push({
+  const isLike = accion === "like" ? 1 : 0;
+
+  // Hacer la solicitud para agregar la reacción
+  fetch("http://127.0.0.1:8080/api/v1/reacciones", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      preguntaId: pregunta.id,
       username: user.username,
       isLike: isLike,
-    });
-    localStorage.setItem("preguntas", JSON.stringify(preguntas));
-    renderPreguntas();
-  }else if(reaccion[foundIndex].isLike === isLike){
-    reaccion.splice(foundIndex,1);
-    localStorage.setItem("preguntas", JSON.stringify(preguntas));
-    renderPreguntas();
-  } 
-  else {
-    alert("Usuario ya ha dado su reacción");
-  }
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error al agregar la reacción");
+      }
+    })
+    .then((updatedPregunta) => {
+      preguntas[index] = updatedPregunta; 
+      // Actualiza la pregunta con las reacciones actualizadas del backend
+      renderPreguntas(); 
+      // Vuelve a renderizar las preguntas en la interfaz
+    })
+    .catch((error) => console.error("Error al agregar reacción:", error));
 }
+
+//funcion para eliminar pregunta con el back
 function eliminarPregunta(index) {
-  preguntas.splice(index, 1);
-  localStorage.setItem("preguntas", JSON.stringify(preguntas));
-  renderPreguntas();
+  const idPregunta = preguntas[index].id;
+  //preguntar sobre la ruta
+  fetch(`http://127.0.0.1:8080/api/v1/preguntas/${idPregunta}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      preguntas.splice(index, 1);
+       // Elimina la pregunta del array local después de eliminarla en el backend
+      renderPreguntas(); 
+      // Vuelve a renderizar las preguntas en la interfaz
+    })
+    .catch((error) => console.error("Error al eliminar la pregunta:", error));
 }
+
+
 function renderPreguntas(filterPregunta) {
   let preguntasHtml = "";
   let preguntasRendered = filterPregunta !== undefined ? filterPregunta : preguntas;
@@ -44,25 +82,39 @@ function renderPreguntas(filterPregunta) {
   preguntasRendered.forEach(function (obj, index) {
     let reacciones = contarReaccion(obj.reaccion);
     preguntasHtml += `
-    <div class="foro__publicadas">
+   <div class="foro__publicadas">
       <div class="foro__pregunta">
-          <img src="../../publics/img/user.png">
-          <p>${obj.username}</p>
-          <button class="foro__reaccion" id="btn__like" onclick="agregarReaccion('${index}', 'like')">${
-      reacciones.likes
-    }<i class='bx bx-like'></i></button>
-          <button class="foro__reaccion" id="btn__dislike" onclick="agregarReaccion(${index}, 'dislike')">${
-      reacciones.dislikes
-    }<i class='bx bx-dislike' ></i></button>
-          <a href="/Frontend/pages/comunidadRespuesta.html?id=${index}" class="foro__ref">
+        <img src="../../publics/img/user.png">
+        <p>${obj.username}</p>
+        <button class="foro__reaccion" id="btn__like" onclick="agregarReaccion('${index}', 'like')">
+          ${reacciones.likes}
+          ${
+            obj.reaccion.some(
+              (r) => r.username === user.username && r.isLike === 1
+            )
+              ? "<box-icon type='solid' name='like'></box-icon>"
+              : "<i class='bx bx-like'></i>"
+          }
+        </button>
+        <button class="foro__reaccion" id="btn__dislike" onclick="agregarReaccion(${index}, 'dislike')">
+          ${reacciones.dislikes}
+          ${
+            obj.reaccion.some(
+              (r) => r.username === user.username && r.isLike === 0
+            )
+              ? "<box-icon type='solid' name='dislike'></box-icon>"
+              : "<i class='bx bx-dislike'></i>"
+          }
+        </button>
+        <a href="/Frontend/pages/comunidadRespuesta.html?id=${index}" class="foro__ref">
           <h3>${obj.pregunta}</h3>
           <p>${obj.tags}</p>
-          </a>
-          ${
-            user && obj.username === user.username
-              ? `<button class="eliminar__post" onclick="eliminarPregunta(${index})"><i class="fa-solid fa-trash"></i></button>`
-              : ""
-          }
+        </a>
+        ${
+          user && obj.username === user.username
+            ? `<button class="eliminar__post" onclick="eliminarPregunta(${index})"><i class="fa-solid fa-trash"></i></button>`
+            : ""
+        }
       </div>
     </div>`;
   });
@@ -83,7 +135,8 @@ function contarReaccion(reaccion) {
 const btnPregunta = document.getElementById("btn__pregunta");
 btnPregunta.addEventListener("click", function () {
   if (!user) {
-    window.location.href = "login.html"; // Redirigir a login.html si el usuario no ha iniciado sesión
+    window.location.href = "login.html"; 
+    // Redirigir a login.html si el usuario no ha iniciado sesión
   }
 });
 function filtro(consulta) {
--- Frontend/Js/post.js
+++ Frontend/Js/post.js
@@ -1,23 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const postForm = document.querySelector(".principal__formulario");
+  const postForm = document.querySelector(".principalformulario");
 
-  postForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  if (postForm) {
+    postForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
 
-    const titulo = document.getElementById("input__titulo").value;
-    const contenido = document.getElementById("input__texto").value;
-    const tags = document.getElementById("input__tags").value;
-    const user = JSON.parse(localStorage.getItem("login_success"));
-    const Preguntas = JSON.parse(localStorage.getItem("preguntas")) || [];
-    Preguntas.push({
-      pregunta: titulo,
-      descripcion: contenido,
-      tags: tags,
-      respuestas: [],
-      username: user.username,
-      reaccion: [],
+      const titulo = document.getElementById("inputtitulo").value;
+      const contenido = document.getElementById("inputtexto").value;
+      const tags = document.getElementById("inputtags").value;
+      const user = JSON.parse(localStorage.getItem("login_success"));
+
+      if (!user) {
+        // Redirigir a la página de inicio de sesión si el usuario no está autenticado
+        window.location.href = "login.html";
+      } else {
+        // Crear un objeto para almacenar la pregunta
+        const preguntaData = {
+          pregunta: titulo,
+          descripcion: contenido,
+          tag: tag,
+          username: user.username,
+          respuestas: [],
+          reaccion: [],
+        };
+
+        try {
+          const response = await fetch(
+            "http://localhost:8080/api/v1/preguntas",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(preguntaData),
+            }
+          );
+
+          if (response.ok) {
+            console.log(response);
+            // Pregunta creada con éxito en la base de datos
+            window.location.href = "comunidad.html";
+          } else {
+            const errorData = await response.json();
+            console.error("Error al crear la pregunta:", errorData.message);
+          }
+        } catch (error) {
+          console.error("Error de red:", error);
+          // Mostrar un mensaje de error
+        }
+      }
     });
-    localStorage.setItem("preguntas", JSON.stringify(Preguntas));
-    window.location.href = "comunidad.html";
-  });
+  }
 });
